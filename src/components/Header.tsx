9328fefb6b3f39742ed700cd3468086c1b47feda
Rob <rverplanke@gmail.com>
2024-11-16 01:51:57 +0100

    chore: link to corresponding components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,15 @@
+import { Link } from 'react-router-dom';
+
 export default function Header() {
   return (
     <header>
       <nav className="z-10 flex justify-center gap-16 items-center shadow-md h-14">
         {/* Make these menu options render dynamicly so that adding new pages will be easier */}
-        <span>Home</span>
-        <span>Kopieren & Printen</span>
-        <span>Drukwerk</span>
-        <span>Kleding & Textiel</span>
-        <span>Contact</span>
+        <Link to="/">Home</Link>
+        <Link to="copy-print">Kopieren & Printen</Link>
+        <Link to="custom-printing">Drukwerk</Link>
+        <Link to="custom-clothing">Kleding & Textiel</Link>
+        <Link to="contact">Contact</Link>
         <select className="p-1" name="language" id="">
           <option value="nl">Nl</option>
           <option value="en">En</option>
