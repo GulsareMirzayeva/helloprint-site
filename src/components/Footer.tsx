82a528e6f79dda98becea6aaca328787f6c1dfde
Rob <rverplanke@gmail.com>
2024-11-16 21:53:02 +0100

    chore: implement translations in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,32 @@
+import { useTranslation } from 'react-i18next';
+
 export default function Footer() {
+  const { t } = useTranslation();
+
   return (
     <div className="flex justify-around h-56 border-t-2 border-[#FB0036] pt-2">
       <div className="flex flex-col">
-        <p className="text-sm font-bold pb-2">Voorwaarden</p>
+        <p className="text-sm font-bold pb-2">{t('footer.agreements.title')}</p>
         <a className="text-xs" href="">
-          Erkenning voorwaarden
+          {t('footer.agreements.acknowledgment')}
         </a>
         <a className="text-xs" href="">
-          Offertes
+          {t('footer.agreements.proposals')}
         </a>
         <a className="text-xs" href="">
-          Bestelling
+          {t('footer.agreements.orders')}
         </a>
         <a className="text-xs" href="">
-          Betaling
+          {t('footer.agreements.payment')}
         </a>
         <a className="text-xs" href="">
-          Prijzen
+          {t('footer.agreements.prices')}
         </a>
         <a className="text-xs" href="">
-          Leveringen
+          {t('footer.agreements.deliveries')}
         </a>
         <a className="text-xs" href="">
-          Klachten
+          {t('footer.agreements.complaints')}
         </a>
       </div>
       <div>
@@ -30,9 +34,9 @@ export default function Footer() {
         <p className="text-xs">Copy & Printshop</p>
         <p className="text-xs">Straatnaam 123</p>
         <p className="text-xs">1234 AB</p>
-        <p className="text-xs">Groningen, Nederland</p>
+        <p className="text-xs">Groningen, {t('footer.adres.countryName')}</p>
         <a className="text-xs" href="">
-          Bekijk op Google Maps
+          {t('footer.adres.visitGoogleMaps')}
         </a>
       </div>
       <div>
@@ -42,13 +46,21 @@ export default function Footer() {
         <p className="text-xs">Facebook</p>
       </div>
       <div>
-        <p className="text-sm font-bold pb-2">Openingstijden</p>
-        <p className="text-xs font-semibold">Maandag - Vrijdag</p>
+        <p className="text-sm font-bold pb-2">
+          {t('footer.openingHours.title')}
+        </p>
+        <p className="text-xs font-semibold">
+          {t('footer.openingHours.midweeks')}
+        </p>
         <p className="text-xs">09:00 - 18:00</p>
-        <p className="text-xs font-semibold">Zaterdag</p>
+        <p className="text-xs font-semibold">
+          {t('footer.openingHours.saturday')}
+        </p>
         <p className="text-xs">10:00 - 17:00</p>
-        <p className="text-xs font-semibold">Zondag</p>
-        <p className="text-xs">Gesloten</p>
+        <p className="text-xs font-semibold">
+          {t('footer.openingHours.sunday')}
+        </p>
+        <p className="text-xs">{t('footer.openingHours.closed')}</p>
       </div>
     </div>
   );
