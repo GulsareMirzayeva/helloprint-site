b2f2a208c48e9fa78b10dda7aca70395360c17e6
Rob <rverplanke@gmail.com>
2024-11-16 03:44:08 +0100

    implement accordion as submenu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,68 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
 export default function Header() {
+  const [isAccordionOpen, setAccordionOpen] = useState(false);
+
+  const handleOpen = () => {
+    setAccordionOpen((prev) => !prev); // Toggle accordion state
+  };
+
   return (
     <header>
-      <nav className="z-10 flex justify-center gap-16 items-center shadow-md h-14">
-        {/* Make these menu options render dynamicly so that adding new pages will be easier */}
+      <nav className="flex justify-center gap-16 items-center shadow-md h-16">
         <Link to="/">Home</Link>
         <Link to="copy-print">Kopieren & Printen</Link>
-        <Link to="custom-printing">Drukwerk</Link>
+
+        {/* Accordion start */}
+        <div className="relative">
+          {/* Drukwerk/Custom printing button */}
+          <div
+            className="flex items-center cursor-pointer"
+            onClick={handleOpen}
+          >
+            <button
+              aria-label="Toggle custom printing submenu"
+              className="mr-2"
+            >
+              Drukwerk
+            </button>
+            {isAccordionOpen ? (
+              <KeyboardArrowUpIcon className="mt-1" />
+            ) : (
+              <KeyboardArrowDownIcon className="mt-1" />
+            )}
+          </div>
+
+          {/* Accordion content */}
+          {isAccordionOpen && (
+            <ul className="absolute top-[calc(100%+2px)] min-w-[110%] mt-4 bg-white shadow-md rounded-sm">
+              <li className="p-2 hover:bg-gray-200">
+                <Link to="/custom-printing/stickers">Stickers</Link>
+              </li>
+              <li className="p-2 hover:bg-gray-200">
+                <Link to="/custom-printing/business-cards">Cards</Link>
+              </li>
+              <li className="p-2 hover:bg-gray-200">
+                <Link to="/custom-printing/flyers">Flyers</Link>
+              </li>
+              <li className="p-2 hover:bg-gray-200">
+                <Link to="/custom-printing/folders">Folders</Link>
+              </li>
+              <li className="p-2 hover:bg-gray-200">
+                <Link to="/custom-printing/posters">Posters</Link>
+              </li>
+            </ul>
+          )}
+        </div>
+        {/* Accordion end */}
+
         <Link to="custom-clothing">Kleding & Textiel</Link>
         <Link to="contact">Contact</Link>
-        <select className="p-1" name="language" id="">
+        <select className="p-1" name="language">
           <option value="nl">Nl</option>
           <option value="en">En</option>
         </select>
