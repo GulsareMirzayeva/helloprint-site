d51a676cd86f513697360e3749fa2663c254c8cd
Rob <rverplanke@gmail.com>
2024-11-19 00:30:22 +0100

    fix: take full width of the page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
   const { t } = useTranslation();
 
   return (
-    <div className="flex justify-around h-56 border-t-2 border-[#FB0036] pt-2">
+    <div className="flex justify-around w-full h-56 border-t-2 border-[#FB0036] py-2">
       <div className="flex flex-col">
         <p className="text-sm font-bold pb-2">{t('footer.agreements.title')}</p>
         <a className="text-xs" href="">
@@ -46,16 +46,16 @@ export default function Footer() {
       <div className="flex flex-col">
         <p className="text-sm font-bold pb-2">Contact</p>
         <p className="text-xs">
-          <PhoneIcon className="mr-2" style={{ fontSize: '14px' }} />
+          <PhoneIcon className="mr-1" style={{ fontSize: '14px' }} />
           050-1234567
         </p>
         <a className="text-xs">
-          <AlternateEmailIcon className="mr-2" style={{ fontSize: '14px' }} />
+          <AlternateEmailIcon className="mr-1" style={{ fontSize: '14px' }} />
           info@mail.nl
         </a>
         <a className="text-xs cursor-pointer">
           <FacebookIcon
-            className="mr-2 text-[#0165E1]"
+            className="mr-1 text-[#0165E1]"
             style={{ fontSize: '14px' }}
           />
           Facebook
