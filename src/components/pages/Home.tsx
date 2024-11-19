c325065e2af34cadf6d9d1f4c58dc50d35cdcd9c
Rob <rverplanke@gmail.com>
2024-11-19 01:46:08 +0100

    feature: add shop info component

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,12 +1,14 @@
 import WelcomeMessage from '../home/WelcomeMessage';
 import ShopCategories from '../home/ShopCategories';
 import Footer from '../Footer';
+import ShopInfo from '../home/ShopInfo';
 
 export default function Home() {
   return (
     <section className="flex flex-col w-full items-center overflow-scroll">
       <WelcomeMessage />
       <ShopCategories />
+      <ShopInfo />
       <Footer />
     </section>
   );
