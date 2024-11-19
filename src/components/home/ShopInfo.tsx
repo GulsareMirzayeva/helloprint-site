93226e7940e5deaec17c37346c80c7bf9a44beb9
Rob <rverplanke@gmail.com>
2024-11-19 17:37:13 +0100

    style: add phone and email icons

--- src/components/home/ShopInfo.tsx
+++ src/components/home/ShopInfo.tsx
@@ -1,11 +1,13 @@
+import PhoneIcon from '@mui/icons-material/Phone';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import { Trans, useTranslation } from 'react-i18next';
 
 export default function ShopInfo() {
   const { t } = useTranslation();
 
   return (
-    <section className="flex items-center justify-center w-full mt-8  bg-[#FB0036]">
-      <div className="flex flex-col justify-center items-end w-full max-w-[800px] h-96 pr-72">
+    <section className="flex items-center justify-center w-full mt-8 bg-[#FB0036]">
+      <div className="flex flex-col justify-center items-end w-full max-w-[800px] h-96 pr-96">
         <div className="flex flex-col items-center text-white ">
           <p className="text-5xl mb-8">{t('shopInfo.openingHours.title')}</p>
           <p className="text-2xl font-semibold">
@@ -22,7 +24,7 @@ export default function ShopInfo() {
           <p className="text-xl">{t('shopInfo.openingHours.closed')}</p>
         </div>
       </div>
-      <div className="flex flex-col justify-center max-w-[800px] h-96 pl-42 bg-[#FB0036]">
+      <div className="flex flex-col justify-center max-w-[800px] h-96 bg-[#FB0036]">
         <div className="flex flex-col items-center text-white ">
           <p className="text-5xl mb-8">{t('shopInfo.contact.title')}</p>
           <p className="font-semibold text-center">
@@ -31,12 +33,18 @@ export default function ShopInfo() {
               components={{ br: <br /> }}
             />
           </p>
-          <p className="pt-8 text-2xl font-semibold">
-            {t('shopInfo.contact.email')}
-          </p>
-          <p className="pt-4 text-2xl font-semibold">
-            {t('shopInfo.contact.telNr')}
-          </p>
+          <div className="flex pt-8">
+            <AlternateEmailIcon className="mt-1 mr-2" />
+            <p className="text-2xl font-semibold">
+              {t('shopInfo.contact.email')}
+            </p>
+          </div>
+          <div className="flex pt-4 ">
+            <PhoneIcon className="mt-1 mr-2" />
+            <p className="text-2xl font-semibold">
+              {t('shopInfo.contact.telNr')}
+            </p>
+          </div>
         </div>
       </div>
     </section>
