199cf97cffba7eb878e6163826408bbb21df2e36
Rob <rverplanke@gmail.com>
2024-11-19 19:46:17 +0100

    style: configurate and use of a gradient

--- src/components/home/ShopInfo.tsx
+++ src/components/home/ShopInfo.tsx
@@ -6,9 +6,9 @@ export default function ShopInfo() {
   const { t } = useTranslation();
 
   return (
-    <section className="flex justify-center w-full mt-8 bg-[#FB0036]">
-      <div className="flex flex-col justify-center items-start w-full max-w-[600px] h-96">
-        <div className="flex flex-col items-center text-white ">
+    <section className="flex justify-center w-full mt-8 bg-gradient-radial from-[#fd3964] via-[#FA2150] to-[#FB0036]">
+      <div className="flex flex-col justify-center items-center min-w-[600px] h-96 p-0 m-0">
+        <div className="flex flex-col items-center text-white">
           <p className="text-5xl mb-8">{t('shopInfo.openingHours.title')}</p>
           <p className="text-2xl font-semibold">
             {t('shopInfo.openingHours.midweeks')}
@@ -24,7 +24,7 @@ export default function ShopInfo() {
           <p className="text-xl">{t('shopInfo.openingHours.closed')}</p>
         </div>
       </div>
-      <div className="flex flex-col justify-center max-w-[600px] h-96 bg-[#FB0036]">
+      <div className="flex flex-col justify-center min-w-[600px] h-96 p-0 m-0">
         <div className="flex flex-col items-center text-white ">
           <p className="text-5xl mb-8">{t('shopInfo.contact.title')}</p>
           <p className="font-semibold text-center">
