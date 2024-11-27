cc962be76507a5611002bdd9db06dda85f10c2de
Rob <rverplanke@gmail.com>
2024-11-28 00:12:40 +0100

    style: close gap between content and footer

--- src/components/pages/CopyPrint.tsx
+++ src/components/pages/CopyPrint.tsx
@@ -4,9 +4,9 @@ import Footer from '../Footer';
 
 export default function CopyPrint() {
   return (
-    <section className="flex flex-col w-full justify-start items-center gap-10 pb-8 overflow-scroll">
+    <section className="flex flex-col w-full justify-start items-center overflow-scroll">
       <IntroductionCopyPrint />
-      <div className="flex justify-center gap-24 py-10 w-full bg-gray-100 ">
+      <div className="flex justify-center gap-24 py-10 mt-10 w-full bg-gray-100 ">
         <PricingTable variant="blackWhite" />
         <PricingTable variant="color" />
       </div>
