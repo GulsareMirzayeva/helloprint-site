f04f9bf83fd74d8b69226dd89a1220fd5e01cd41
Rob <rverplanke@gmail.com>
2025-01-16 19:54:51 +0100

    refactor: prevent repetitive code by using map method

--- src/components/termsOfSale/TermsOfSaleContent.tsx
+++ src/components/termsOfSale/TermsOfSaleContent.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import { TextBlock } from '../elementTemplates/TextBlock';
 import { useData } from '../../context/DataContext';
+import { terms } from '../../lib/termsOfSaleItems';
 
 export default function TermsOfSaleContent() {
   useTranslation();
@@ -13,54 +14,20 @@ export default function TermsOfSaleContent() {
           <TextBlock value="termsOfSale.title" variant="title" />
         </div>
 
-        {/* Term one */}
+        {/* Display all terms from the termsOfSaleItems list, highlight the background if the term is active */}
 
-        <div className={activeTerm == 'term-1' ? 'p-4 bg-gray-100' : 'p-4'}>
-          <TextBlock value="termsOfSale.term-1.title" variant="bold" />
-          <TextBlock value="termsOfSale.term-1.termContent" variant="body" />
-        </div>
-
-        {/* Term Two */}
-
-        <div className={activeTerm == 'term-2' ? 'p-4 bg-gray-100' : 'p-4'}>
-          <TextBlock value="termsOfSale.term-2.title" variant="bold" />
-          <TextBlock value="termsOfSale.term-2.termContent" variant="body" />
-        </div>
-
-        {/* Term Three */}
-
-        <div className={activeTerm == 'term-3' ? 'p-4 bg-gray-100' : 'p-4'}>
-          <TextBlock value="termsOfSale.term-3.title" variant="bold" />
-          <TextBlock value="termsOfSale.term-3.termContent" variant="body" />
-        </div>
-
-        {/* Term Four */}
-
-        <div className={activeTerm == 'term-4' ? 'p-4 bg-gray-100' : 'p-4'}>
-          <TextBlock value="termsOfSale.term-4.title" variant="bold" />
-          <TextBlock value="termsOfSale.term-4.termContent" variant="body" />
-        </div>
-
-        {/* Term Five */}
-
-        <div className={activeTerm == 'term-5' ? 'p-4 bg-gray-100' : 'p-4'}>
-          <TextBlock value="termsOfSale.term-5.title" variant="bold" />
-          <TextBlock value="termsOfSale.term-5.termContent" variant="body" />
-        </div>
-
-        {/* Term Six */}
-
-        <div className={activeTerm == 'term-6' ? 'p-4 bg-gray-100' : 'p-4'}>
-          <TextBlock value="termsOfSale.term-6.title" variant="bold" />
-          <TextBlock value="termsOfSale.term-6.termContent" variant="body" />
-        </div>
-
-        {/* Term Seven */}
-
-        <div className={activeTerm == 'term-7' ? 'p-4 bg-gray-100' : 'p-4'}>
-          <TextBlock value="termsOfSale.term-7.title" variant="bold" />
-          <TextBlock value="termsOfSale.term-7.termContent" variant="body" />
-        </div>
+        {terms.map(({ term }) => (
+          <div
+            key={term}
+            className={activeTerm == term ? 'p-4 bg-gray-100' : 'p-4'}
+          >
+            <TextBlock value={`termsOfSale.${term}.title`} variant="bold" />
+            <TextBlock
+              value={`termsOfSale.${term}.termContent`}
+              variant="body"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
