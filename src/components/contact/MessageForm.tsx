3fa07fe960598a2a9178348a0cf4e9a707d750f8
Rob <rverplanke@gmail.com>
2025-01-20 17:17:50 +0100

    refactor: improve form accessibility
    Set focus on subject input field after render.
    Add labels to inputfields according to accessibility guidelines.
    Control the input components by managing them with a state.

--- src/components/contact/MessageForm.tsx
+++ src/components/contact/MessageForm.tsx
@@ -1,22 +1,67 @@
 import { useTranslation } from 'react-i18next';
 import { TextBlock } from '../elementTemplates/TextBlock';
+import { useEffect, useRef, useState } from 'react';
 
 export default function MessageForm() {
   // Subscribes the component to i18n language so when the language changes, the content will be translated inmediatly
   const { t } = useTranslation();
+
+  // Controlled components - Control the values of the input fields
+  const [subjectValue, setSubjectValue] = useState<string>();
+  const [messageValue, setMessageValue] = useState<string>();
+
+  // Set focus on the 'Subject' input field when the component is loaded
+  const subjectInputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    subjectInputRef.current?.focus();
+  }, []);
+
+  // Update the input values while text is being typed
+  const handleOnChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    e.target.id === 'subject'
+      ? setSubjectValue(e.target.value)
+      : setMessageValue(e.target.value);
+  };
+
   return (
     <form className="flex flex-col pt-8">
+      {/* Subject input field */}
       <div className="mb-2">
-        <TextBlock value="contactPage.messageSubject" variant="body" />
+        <label htmlFor="subject">
+          <TextBlock value="contactPage.messageSubject" variant="body" />
+        </label>
+
         <input
+          ref={subjectInputRef}
           type="text"
+          id="subject"
+          name="subject"
           className="w-[400px] rounded-md mt-1 p-1 pl-2 border"
+          value={subjectValue}
+          onChange={handleOnChange}
         />
       </div>
+
+      {/* message input field */}
       <div className="mb-2">
-        <TextBlock value="contactPage.messageContent" variant="body" />
-        <textarea className="rounded-md w-[400px] h-[140px] mt-1 p-1 pl-2 border" />
+        <label htmlFor="message">
+          <TextBlock value="contactPage.messageContent" variant="body" />
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          className="rounded-md w-[400px] h-[140px] mt-1 p-1 pl-2 border"
+          value={messageValue}
+          onChange={handleOnChange}
+        />
       </div>
+
+      {/* Send button */}
       <button
         type="submit"
         className="relative w-full mt-1 h-10 rounded-md bg-[#FB0036] text-white font-bold transition-transform subpixel-antialiased duration-200 ease-in hover:scale-105"
--- src/components/elementTemplates/PricingTable.tsx
+++ src/components/elementTemplates/PricingTable.tsx
@@ -7,45 +7,31 @@ type PricingTableTypes = {
   prices: PricingOptions[] | number[];
 };
 
-// Generate a table with dynamic size and content:
-
-// headerTitles:
-// The titles of each column at the top of the table
-// (for example: ["Prints", "Budget", "High quality"])
-
-// units:
-// Show the used units in the column below the titles
-// (for example: ["Amount", "Price per print", "Price per print"])
-
-// options:
-// Prices are categorized by the amount or the type of a medium (paper/clothing)
-// (for example: ['1-100', '101-250', '251-500', '501-1000'])
-
-// prices:
-// An object that contains the prices for each option
-// (for example: { "1-00": {"budget": x.xx, "hq": x.xx} })
-
 export default function PricingTable({
   headerTitles,
   units,
   options,
   prices,
 }: PricingTableTypes) {
-  // Prepare data for easy acces in the table
+  // Prepare data for easy access in the table
   const tableData = options.map((option, index) => ({
     option,
     prices: prices[index] || [],
   }));
 
   return (
-    <table className="table-auto">
+    <table className="table-auto" role="table">
+      <caption className="sr-only">
+        Table showing pricing options with their corresponding units and prices.
+      </caption>
       <thead>
         <tr>
-          {/* Create a headers cell for each given header title */}
+          {/* Create a header cell for each given header title */}
           {headerTitles.map((title) => (
             <th
               key={title}
               className="border bg-[#FB0036] text-white border-white py-2 px-4 text-left"
+              scope="col"
             >
               <div className="text-white">{title}</div>
             </th>
@@ -54,24 +40,28 @@ export default function PricingTable({
       </thead>
 
       <tbody>
+        {/* Row for units */}
         <tr>
-          {/* At the start of the table create a row that contains the used units */}
           {units.map((unit, index) => (
-            <td key={index} className="border border-gray-300 px-4 py-2">
+            <th
+              key={index}
+              className="border border-gray-300 px-4 py-2"
+              scope="row"
+            >
               <i>{unit}</i>
-            </td>
+            </th>
           ))}
         </tr>
 
-        {/* Create rows for each option and corresponding prices */}
+        {/* Rows for options and corresponding prices */}
         {tableData.map((row, rowIndex) => {
           const rowPrices = [...Object.values(row.prices)];
 
           return (
             <tr key={rowIndex} className="border odd:bg-white even:bg-gray-50">
-              <td className="border border-gray-300 px-4 py-2">
+              <th className="border border-gray-300 px-4 py-2" scope="row">
                 <b>{row.option}</b>
-              </td>
+              </th>
 
               {rowPrices.map((price, priceIndex) => (
                 <td
