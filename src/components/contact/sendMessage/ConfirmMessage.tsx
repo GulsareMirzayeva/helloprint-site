f9014c0ce6dbd0ebdc9e493edc4c69ef87149a0a
Rob <rverplanke@gmail.com>
2025-02-01 21:37:40 +0100

    fix: add more spacing, correct typo

--- src/components/contact/sendMessage/ConfirmMessage.tsx
+++ src/components/contact/sendMessage/ConfirmMessage.tsx
@@ -3,8 +3,8 @@ import { Trans } from 'react-i18next';
 
 export function ConfirmMessage() {
   return (
-    <div className="flex justify-start items-center text-center min-w-[400px] md:min-w-[500px] w-full pt-12">
-      <div className="bg-green-100 text-green-800 border border-green-300 rounded-lg p-2 py-4 shadow-md">
+    <div className="flex justify-start items-center text-center min-w-[300px] md:min-w-[500px] w-full pt-12">
+      <div className="bg-green-100 text-green-800 border border-green-300 rounded-lg p-4 py-4 shadow-md">
         <Trans
           i18nKey={contactFormTextContentPaths.confirmMessage}
           components={{ br: <br />, b: <b /> }}
