ef57edf611ed669ac600a8fa34853273fe505ed0
Rob <rverplanke@gmail.com>
2025-02-24 16:25:27 +0100

    chore: add credits beneath footer

--- src/components/elementTemplates/FooterCopyright .tsx
+++ src/components/elementTemplates/FooterCopyright .tsx
@@ -2,6 +2,16 @@ export function FooterCopyright() {
   return (
     <div className="flex w-full justify-center pb-1">
       <div className="flex text-xs">© 2025 Copy en Printshop 't Hartje</div>
+      <div className="text-xs text-gray-400">
+        &nbsp;|&nbsp;Built by&nbsp;
+        <a
+          className="cursor-pointer"
+          href="http://www.robverplankejr.nl"
+          target="_blank"
+        >
+          Rob Verplanke
+        </a>
+      </div>
     </div>
   );
 }
